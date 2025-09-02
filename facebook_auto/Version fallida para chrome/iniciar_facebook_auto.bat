122d1fe913321dfdce5f227306b67ab99a7363da
wellinmartlat <wellington.martinez@latinus.net>
2025-09-02 10:13:06 -0500

    Corrección de nombres de archivos y del repositorio

--- facebook_auto/Version fallida para chrome/iniciar_facebook_auto.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Iniciando script de publicación automática en Facebook...
-
-:: Usar el comando 'py' que sabemos que funciona
-py "C:\Users\welli\OneDrive\Documents\Repositorios\facebook_auto\facebook_auto_post.py"
-
-echo Script completado.
---
+++ iniciar_publicacion_automatica.bat
@@ -0,0 +1,6 @@
+@echo off
+echo Iniciando script de publicación automática en Facebook...
+
+py "C:\Users\welli\OneDrive\Documents\Repositorios\automatizacion_mensajes_biblicos\automatizacion_mensajes_biblicos.py"
+
+echo Script completado.
